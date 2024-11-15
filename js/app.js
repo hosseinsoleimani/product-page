304cb02524dc2c9dd275003f7d36026c5548328c
HosseinSoleimani <mrhosseinsoleimani@gmail.com>
2024-11-15 20:20:50 +0330

    filter handler updated

--- js/app.js
+++ js/app.js
@@ -2,6 +2,16 @@ const searchInput = document.getElementById("search-input");
 const products = document.querySelectorAll(".product-item");
 const buttons = document.querySelectorAll(".filter");
 
+const changeClass = (filter) => {
+  buttons.forEach((button) => {
+    if (button.dataset.filter === filter) {
+      button.classList.add("selected");
+    } else {
+      button.classList.remove("selected");
+    }
+  });
+};
+
 const searchHandler = (event) => {
   const searchValue = event.target.value.toLowerCase().trim();
   products.forEach((product) => {
@@ -14,14 +24,24 @@ const searchHandler = (event) => {
   });
 };
 
+const filterHandler = (event) => {
+  const filter = event.target.dataset.filter;
 
-const filterHandler = (event)=>{
-const filter = event.target.dataset.filter;
-}
+  changeClass(filter);
 
+  products.forEach((product) => {
+    const category = product.dataset.category;
 
+    if (filter === "all") {
+      product.style.display = "block";
+    } else
+      filter === category
+        ? (product.style.display = "block")
+        : (product.style.display = "none");
+  });
+};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", filterHandler);
+});
 searchInput.addEventListener("keyup", searchHandler);
-
-buttons.forEach((button)=>{
-  button.addEventListener("click",filterHandler)
-})
